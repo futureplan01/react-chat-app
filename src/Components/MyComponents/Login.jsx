56bbc92782744d5d05c944c3801191a030e0a9a8
Mark Sayong <mbenneth7@yahoo.com>
2019-10-19 19:09:09 -0400

    change login page

--- src/Components/MyComponents/Login.jsx
+++ src/Components/MyComponents/Login.jsx
@@ -11,14 +11,53 @@ class Login extends Component {
       userName: "",
       email: "",
       signUp: false,
-      error: ''
+      error: '',
+      menu: false
     };
     
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleSignUp = this.handleSignUp.bind(this);
+    this.handleMenu = this.handleMenu.bind(this);
+    this.handleCarousel = this.handleCarousel.bind(this);
   }
+    
+  handleCarousel(event){
+    const images = document.querySelectorAll(".slide-images");
+    const timer = 5000;
+    let currentImageCounter = 0;
 
+    images[currentImageCounter].classList.add("displayImage");
 
+    setInterval(nextImage, timer);
+
+    function nextImage(){
+
+        images[currentImageCounter].classList.add("stackImage");
+
+        /*USED TO MAKE SURE BACKGROUND DOESN'T INTERFERE/SHOW WITH IMAGE TRANSITIONS*/
+
+            const tempCounter = currentImageCounter;
+
+            setTimeout(function(){
+                images[tempCounter].classList.remove("displayImage");
+            },1000);
+
+        /*END*/
+
+        currentImageCounter = (currentImageCounter + 1) % images.length;
+
+        images[currentImageCounter].classList.add("displayImage");
+        images[currentImageCounter].classList.remove("stackImage"); 
+    }
+  }
+  handleMenu(event){
+      if(this.state.menu === false)
+          this.setState({menu: true})
+      
+      else
+          this.setState({menu:false})
+  }
+    
   handleSubmit(events) {
     events.preventDefault();
 
@@ -40,7 +79,12 @@ class Login extends Component {
     events.preventDefault();
     this.setState({ signUp: true });
   }
+    
+  componentDidMount(){
+      this.handleCarousel();
+  }
   render() {
+    let cssMenu = '';
     if (this.state.signUp) {
       return <Redirect push to="/SignUp" />;
     }
@@ -49,27 +93,68 @@ class Login extends Component {
       console.log('Auth is: '+ this.props.isAuth);
       return <Redirect push to="/Home" />; 
     }
+    if(this.state.menu){
+        cssMenu = "showing";
+    }
     let error;
     if (this.state.problem) {
       error = <Problem/>;
     }
     return( 
-    <div>
-      <Header name='Sign Up'/>
-      <center id = 'loginForm'>
-          <h1>Lets Start Chatting</h1>
-          {error}
-          <form className="form" onSubmit={this.handleSubmit}>
-          <input className ='input' type="text" name="email" placeholder="Email" />
-            <br />
-          <input className='input' type="password" name="password" placeholder="Password" />
-            <br />
-          <input id='submitBtn' className='input' type="submit" />
-          </form>
-        <h2 className= 'yellow'> OR </h2>
-        <button id='signUpBtn' className='input' onClick={this.handleSignUp}>SignUp</button>
-        </center>
-    </div>);
+    <div  className = "container">		
+
+			<header>
+				<nav>
+					<div className = 'menu-icon' onClick ={this.handleMenu}>
+						<i className="fas fa-bars fa-2x"></i>
+					</div>
+
+					<h1 className = "logo">
+						majestic vibes
+					</h1>	
+
+					<div className = "menu">
+						<ul className = {cssMenu}>
+							<li><a href = "#">Sign-Up</a></li>
+							<span className = "divider"></span>
+                            <li><a herf = "#">Log-In</a></li>
+                        </ul>
+					</div>		 	
+				</nav>
+			</header>
+
+			<div id = "slider">
+				<div className = "slide-images slide1"></div>
+				<div className = "slide-images slide2"></div>
+				<div className = "slide-images slide3"></div>
+				<div className = "slide-images slide4"></div>
+				<div className = "slide-images slide5"></div>
+			</div>
+
+			
+			<div className = "login-box">
+
+			<form>
+
+				<h1>Login</h1>
+
+				<div className = "textbox">
+					<i className="fas fa-user"></i>
+                    <input type = "text" placeholder="Username" name = "" value = ""/>
+				</div>
+
+				<div className = "textbox">
+					<i className="fas fa-lock"></i>
+					<input type = "password" placeholder="Password" name = "" value = ""/>
+				</div>
+
+				<input className = "btn" type = "button" name = "" value = "Sign-in"/>
+
+			</form>
+
+			</div>
+
+		</div>);
   }
 }
 
