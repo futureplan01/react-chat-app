7fea9545898ff91b5c3fa43e262bc8a9e541532d
Jesaiah Prayor <JPrayor@dcas.nyc.gov>
2019-10-22 16:15:55 -0400

    fixed header

--- src/Components/MyComponents/LoginContainer.jsx
+++ src/Components/MyComponents/LoginContainer.jsx
@@ -5,60 +5,13 @@ import Header from "./Header";
 import Slider from "./Slider";
 import Login from "./Login";
 
-class LoginContainer extends Component {
-  constructor() {
-    super();
-    this.state = {
-      userName: "",
-      email: "",
-      menu: false
-    };
-
-    this.handleCarousel = this.handleCarousel.bind(this);
-  }
-    
-  handleCarousel(event){
-    const images = document.querySelectorAll(".slide-images");
-    const timer = 5000;
-    let currentImageCounter = 0;
-
-    images[currentImageCounter].classList.add("displayImage");
-
-    setInterval(nextImage, timer);
-
-    function nextImage(){
-
-        images[currentImageCounter].classList.add("stackImage");
-
-        /*USED TO MAKE SURE BACKGROUND DOESN'T INTERFERE/SHOW WITH IMAGE TRANSITIONS*/
-
-            const tempCounter = currentImageCounter;
-
-            setTimeout(function(){
-                images[tempCounter].classList.remove("displayImage");
-            },1000);
-
-        /*END*/
-
-        currentImageCounter = (currentImageCounter + 1) % images.length;
-
-        images[currentImageCounter].classList.add("displayImage");
-        images[currentImageCounter].classList.remove("stackImage"); 
-    }
-  }
-
-
-  componentDidMount(){
-      this.handleCarousel();
-  }
-  render() {
+function LoginContainer() {
     return( 
     <div  className = "container">
       <Header/>
       <Slider/>
       <Login/>
 		</div>);
-  }
 }
 
 export default LoginContainer;
--- src/Components/MyComponents/Slider.jsx
+++ src/Components/MyComponents/Slider.jsx
@@ -1,43 +1,54 @@
-import React from "react";
+import React, {Component} from "react";
+import { tsThisType } from "@babel/types";
 
-function handleCarousel(event){
-    const images = document.querySelectorAll(".slide-images");
-    const timer = 5000;
-    let currentImageCounter = 0;
+class Slider extends Component{
 
-    images[currentImageCounter].classList.add("displayImage");
-
-    setInterval(nextImage, timer);
-
-    function nextImage(){
-
-        images[currentImageCounter].classList.add("stackImage");
-
-        /*USED TO MAKE SURE BACKGROUND DOESN'T INTERFERE/SHOW WITH IMAGE TRANSITIONS*/
-
-            const tempCounter = currentImageCounter;
-
-            setTimeout(function(){
-                images[tempCounter].classList.remove("displayImage");
-            },1000);
-
-        /*END*/
-
-        currentImageCounter = (currentImageCounter + 1) % images.length;
-
-        images[currentImageCounter].classList.add("displayImage");
-        images[currentImageCounter].classList.remove("stackImage"); 
+    constructor (){
+        super();
+        this.handleCarousel = this.handleCarousel.bind(this);
     }
-}
-
-function Slider (props){
-    return (<div id = "slider">
-    <div className = "slide-images slide1"></div>
-    <div className = "slide-images slide2"></div>
-    <div className = "slide-images slide3"></div>
-    <div className = "slide-images slide4"></div>
-    <div className = "slide-images slide5"></div>
-    </div>);
+    handleCarousel(event){
+        const images = document.querySelectorAll(".slide-images");
+        const timer = 5000;
+        let currentImageCounter = 0;
+    
+        images[currentImageCounter].classList.add("displayImage");
+    
+        setInterval(nextImage, timer);
+    
+        function nextImage(){
+    
+            images[currentImageCounter].classList.add("stackImage");
+    
+            /*USED TO MAKE SURE BACKGROUND DOESN'T INTERFERE/SHOW WITH IMAGE TRANSITIONS*/
+    
+                const tempCounter = currentImageCounter;
+    
+                setTimeout(function(){
+                    images[tempCounter].classList.remove("displayImage");
+                },1000);
+    
+            /*END*/
+    
+            currentImageCounter = (currentImageCounter + 1) % images.length;
+    
+            images[currentImageCounter].classList.add("displayImage");
+            images[currentImageCounter].classList.remove("stackImage"); 
+        }
+    }
+    componentDidMount(){
+        this.handleCarousel();
+    }
+    render(){
+        return (
+            <div id = "slider">
+        <div className = "slide-images slide1"></div>
+        <div className = "slide-images slide2"></div>
+        <div className = "slide-images slide3"></div>
+        <div className = "slide-images slide4"></div>
+        <div className = "slide-images slide5"></div>
+        </div>);
+    }
 }
 
 export default Slider;
