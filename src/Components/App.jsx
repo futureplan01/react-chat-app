5b45fa74d481464f123b381f1edb69a1e7ab0cfe
Jesaiah <jesaiahprayor@gmail.com>
2019-10-26 02:08:07 -0400

    remove homepage

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,8 +1,6 @@
 import React, {Component} from 'react';
 import { BrowserRouter as Router} from 'react-router-dom';
 import { Switch, Route } from "react-router-dom";
-import Home from "./MyComponents/HomePage";
-
 import LoginContainer from "./MyComponents/LoginContainer";
 import SignUp from "./MyComponents/SignUpContainer";
 
@@ -15,7 +13,6 @@ class App extends Component {
           <Route exact path="/" render={() => 
             <LoginContainer />
           }/>
-          <Route exact path="/Home" render={() => <Home  />}/>
           <Route exact path="/SignUp" component={SignUp} />
         </Switch>
         </Router>);
